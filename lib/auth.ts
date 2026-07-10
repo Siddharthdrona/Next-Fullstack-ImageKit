5163b9383a54d9c3807aa1eaf288f17cf7af19da
Siddharth Dronachary <siddudrona@gmail.com>
2026-07-10 22:36:11 +0530

    add NextAuth configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -64,5 +64,16 @@ export const authOptions: NextAuthOptions = {
       return session;
     }
     // Can be added SIGN In and redirect
-  }
+  },
+
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+
 }
